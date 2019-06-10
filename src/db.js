7c4f3c62740f9a0753fefefd2241fe79895dc67e
Manu Hdez <manu.hdez.dev@gmail.com>
2019-06-10 21:01:41 +0100

    #13 our first query and mutation

--- src/db.js
+++ src/db.js
@@ -1,11 +1,11 @@
 // This file connects us to the remote prisma DB and gives us the interface to query it with js
-const {Prisma} = require('prisma-binding');
+const { Prisma } = require('prisma-binding');
 
 const db = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
   endpoint: process.env.PRISMA_ENDPOINT,
   secret: process.env.PRISMA_SECRET,
-  debug: false,
+  debug: false
 });
 
 module.exports = db;
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const Mutations = {};
+const Mutations = {
+  createDog(parent, args, context, info) {
+    // create a dog!
+    global.dogs = global.dogs || [];
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+    console.log(args);
+  }
+};
 
 module.exports = Mutations;
